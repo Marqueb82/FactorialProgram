2c7117c51a7513235cc66179e78846a2f5b55e2a
Marque Burgess <mburg22@gmail.com>
2018-07-16 23:27:37 -0400

    Add project files.

---
+++ FactorialProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProgram
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 0) 
+            {
+                return 1;
+            }
+            else
+            {
+                return (n * Factorial(n - 1)); //take value and multiply out
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int factorThis;
+
+            Console.WriteLine("Enter number to factor:  ");
+            factorThis = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(factorThis + " factored out is equal to " + Factorial(factorThis));
+
+            Console.ReadKey();
+        }
+    }
+}
